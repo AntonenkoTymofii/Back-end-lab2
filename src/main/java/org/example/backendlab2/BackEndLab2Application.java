320563dfc84c31032c1d7d9f92613c6bb39f186f
AntonenkoTymofii <antonenkotimofii@gmail.com>
2024-10-27 21:14:51 +0200

    initial commit

---
+++ src/main/java/org/example/backendlab2/BackEndLab2Application.java
@@ -0,0 +1,13 @@
+package org.example.backendlab2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndLab2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndLab2Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/backendlab2/BackEndLab2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.backendlab2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndLab2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
