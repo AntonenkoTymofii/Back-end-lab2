29b7b23cc55a225dbfea5c98b37d9674815def0e
AntonenkoTymofii <antonenkotimofii@gmail.com>
2024-11-21 19:52:19 +0200

    update repositories for searching models

--- src/main/java/org/example/backendlab2/repository/PersonRepository.java
+++ src/main/java/org/example/backendlab2/repository/PersonRepository.java
@@ -4,4 +4,5 @@ import org.example.backendlab2.model.Person;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PersonRepository extends CrudRepository<Person, Long> {
+    Person findByName(String name);
 }
--- src/main/java/org/example/backendlab2/repository/RecordRepository.java
+++ src/main/java/org/example/backendlab2/repository/RecordRepository.java
@@ -3,5 +3,11 @@ package org.example.backendlab2.repository;
 import org.example.backendlab2.model.Record;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface RecordRepository extends CrudRepository<Record, Long> {
+    List<Record> findByPerson_Id(Long id);
+    List<Record> findByPerson_Name(String name);
+    List<Record> findByCategory_Id(Long id);
+    List<Record> findByCategory_Name(String name);
 }
